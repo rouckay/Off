7201af139b570a3e2fadb6cc569ec33813d212e4
Farhad Rahmani <55535348+2019farhad@users.noreply.github.com>
2021-11-19 10:36:50 +0430

    Updates

---
+++ precache-manifest.8b14f9b48f484d13fc37b65720376d61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e385bc5895cd9920ac64d6b80814b2ee",
+    "url": "/Off/index.html"
+  },
+  {
+    "revision": "5b6da3db2e0a97373d5f",
+    "url": "/Off/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "026487e6693b55d79e02",
+    "url": "/Off/static/js/2.b6f1219b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Off/static/js/2.b6f1219b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b6da3db2e0a97373d5f",
+    "url": "/Off/static/js/main.ece541c7.chunk.js"
+  },
+  {
+    "revision": "12024a58102b721484dd",
+    "url": "/Off/static/js/runtime-main.617da288.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Off/static/media/logo.5d5d9eef.svg"
+  }
+]);
